5fc291449f236633f25202dd2305c1121600e60f
vtranduc <vtranduc@ryerson.ca>
2019-10-15 23:27:07 -0400

    Updates

--- precache-manifest.e6dedc8ae5666ced4aa029054d3d3ceb.js
+++ precache-manifest.e6dedc8ae5666ced4aa029054d3d3ceb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "67bb24f7f41ed85eb04f4bbf20891c8b",
+    "revision": "1506be902ae37fcc64db41fc63925c61",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8c38a9d5a034b752815a",
-    "url": "/portfolio/static/css/main.f8442f98.chunk.css"
+    "revision": "912a0d6c7d6748c05043",
+    "url": "/portfolio/static/css/main.638269cb.chunk.css"
   },
   {
     "revision": "43cf5747d5804a84f540",
     "url": "/portfolio/static/js/2.6115e29a.chunk.js"
   },
   {
-    "revision": "8c38a9d5a034b752815a",
-    "url": "/portfolio/static/js/main.64af1e75.chunk.js"
+    "revision": "912a0d6c7d6748c05043",
+    "url": "/portfolio/static/js/main.96c46fab.chunk.js"
   },
   {
     "revision": "791f997dc0acbb20fb93",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6a541879785bafc1f8f8758f0f0f27ce.js"
+  "/portfolio/precache-manifest.e6dedc8ae5666ced4aa029054d3d3ceb.js"
 );
 
 self.addEventListener('message', (event) => {
