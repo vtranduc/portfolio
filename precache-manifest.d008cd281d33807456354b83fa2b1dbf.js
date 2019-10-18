ead31ea7ac9041ef7dcd8860b539a0dcc2a30aee
vtranduc <vtranduc@ryerson.ca>
2019-10-18 06:46:26 -0400

    Updates

---
+++ precache-manifest.d008cd281d33807456354b83fa2b1dbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6738a9ac2b43fc8a8ad686548f32c6d8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "883529b22d42b261db34",
+    "url": "/portfolio/static/css/main.cdb2f8cb.chunk.css"
+  },
+  {
+    "revision": "e3f91b68c2f677823e54",
+    "url": "/portfolio/static/js/2.8f07cd57.chunk.js"
+  },
+  {
+    "revision": "883529b22d42b261db34",
+    "url": "/portfolio/static/js/main.ec4b6e59.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "4d740888825ff6e44028919c3f2ebfc0",
+    "url": "/portfolio/static/media/stars.4d740888.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.24361381667237da9cc344bbd50172c3.js"
+  "/portfolio/precache-manifest.d008cd281d33807456354b83fa2b1dbf.js"
 );
 
 self.addEventListener('message', (event) => {
